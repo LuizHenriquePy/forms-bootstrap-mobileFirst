142bdd6c4f5f5d97c6138ba5517b6602ee4f21fa
Luiz Henrique <luizhenriquemendes123123@gmail.com>
2022-06-07 16:58:28 -0300

    requisito 10

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function getValues() {
   const inputEmail = document.getElementById('header-email');
   const inputPassword = document.getElementById('header-password');
   if (inputEmail.value === 'tryber@teste.com' && inputPassword.value === '123456') {
+    inputEmail.value = '';
+    inputPassword.value = '';
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
-  inputEmail.value = '';
-  inputPassword.value = '';
 }
 
 const headerButton = document.getElementById('header-button');
